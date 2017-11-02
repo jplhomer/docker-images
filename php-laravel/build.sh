96c719685f31b828effbafa588c10c0e96b9d706
Josh Larson <jplhomer@gmail.com>
2017-11-02 17:22:40 -0500

    Tag latest, and install Composer better

--- php-laravel/build.sh
+++ php-laravel/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 php_versions=("7" "7.1" "7.2-rc")
+
+# Build the latest version
+docker build . -t jplhomer/php-laravel:latest
+docker push jplhomer/php-laravel:latest
+
+# Build the other specific versions
 for php in ${php_versions[@]}
 do
 	tag="jplhomer/php-laravel:$php"
